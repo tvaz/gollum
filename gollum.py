0d62b69d12d27c39884d99693405578201fa6feb
tvaz <victoriavaz24@gmail.com>
2018-10-15 23:02:19 +0000

    Fixes some bugs, adds a logger, gitignore, requirements.txt and breaks out data into a json

--- gollum.py
+++ gollum.py
@@ -5,13 +5,29 @@ import re
 import discord
 from discord.ext import commands
 from _token import token
+import json
+import logging
+import datetime
+
+logger = logging.getLogger("gollum")
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch_format = logging.Formatter('%(asctime)s - %(message)s')
+logger.addHandler(ch)
+log_file = "/home/bots/gollum/gollum_" + datetime.datetime.today().strftime("%m_%d") + ".log"
+fh = logging.FileHandler(log_file)
+fh.setLevel(logging.DEBUG)
+fh_format = logging.Formatter('%(asctime)s - %(lineno)d - %(levelname)-8s - %(message)s')
+fh.setFormatter(fh_format)
+logger.addHandler(fh)
 
 bot = commands.Bot(command_prefix="!")
 active_riddle = ""
-riddles = {"What has roots as nobody sees,\nIs taller than trees\nUp, up it goes,\nAnd yet never grows?":"A mountain",
-"Voiceless it cries,\nWingless flutters,\nToothless bites,\nMouthless mutters.":"Wind",
-"It cannot be seen, cannot be felt.\nCannot be heard, cannot be smelt.\nIt lies behind stars and under hills,\nAnd empty holes it fills.\nIt comes first and follows after,\nEnds life, kills laughter.":"Dark",
-"Alive without breath,\nAs cold as death;\nNever thirsty, ever drinking,\nAll in mail never clinking.":"Fish"}
+riddles = {}
+
+with open('/home/bots/gollum/riddles.json') as riddle_data:
+	riddles = json.load(riddle_data)
 
 @bot.event
 async def on_ready():
@@ -30,21 +46,30 @@ async def precious():
 @bot.command()
 async def riddle():
     global active_riddle
+    logger.info("Asking the following riddle...")
     active_riddle = random.choice(list(riddles.keys()))
+    logger.info(active_riddle)
     await bot.say(active_riddle)
 
 @bot.command()
 async def guess(guess : str):
     global active_riddle
+    logger.debug("Recieved guess for Active riddle:")
+    logger.debug(active_riddle)
+    logger.debug("The guess was:    "+guess)
     if active_riddle == "":
+        logger.debug("Guess was empty!")
         await bot.say("Hobbits got to ask a question first...")
         return
     answer = riddles[active_riddle]
+    logger.debug("Expected answer was:   " + answer)
     if answer == guess:
+        logger.debug("Success!")
         await bot.say("Yessss....correct...")
         active_riddle = ""
     else:
-        phrase = random.choice(["I wonder what hobbitses taste like...", "Wrong wrong wrong!!", "Munch crunch munch..."])
+        logger.debug("Failure")
+        phrase = random.choice(["Wrong wrong wrong!!", "Munch crunch munch..."])
         await bot.say(phrase)
 
 bot.run(token)
